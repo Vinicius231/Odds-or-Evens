cd3eae9ecf41dd655c30bbb56e4fe13428a62325
Vinicius231 <103453815+Vinicius231@users.noreply.github.com>
2022-05-06 19:17:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 function clicou() {
     var evodd = document.getElementById('evodd')
+
     var number = window.prompt('Enter some number:')
+
     var parim = number%2
+
     if (parim == 0) {
+
         evodd.innerHTML += `<p>The number ${number} entered is <b>EVEN.</b>` 
     }else {
         evodd.innerHTML += `<p> The number ${number} entered is <b>ODD.</b>`
     } 
+
+
+
+
+
     
 }
